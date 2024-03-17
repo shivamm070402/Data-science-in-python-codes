501bce0e73efa064e6ce6b666777cb9c9af3f1e2
shivamm070402 <108880698+shivamm070402@users.noreply.github.com>
2024-03-17 06:34:41 -0700

    Add files via upload

---
+++ 2darray.py
@@ -0,0 +1,6 @@
+#2d array
+import numpy as np 
+a = np.array([[1,2],[3,4],[5,6]], dtype = np.float64) # specify data type while creating an array
+print(a.itemsize)#itemsize means the number of items in the array along each dimension (in bytes)
+print(a.ndim)# ndim means number of dimensions in the array   
+print(a)
