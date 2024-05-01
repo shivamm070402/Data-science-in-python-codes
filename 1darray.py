b33ab35493a14e06e68fbec78934111303c71538
shivamm070402 <108880698+shivamm070402@users.noreply.github.com>
2024-05-01 08:49:37 -0700

    Delete 1darray.py

--- 1darray.py
+++
@@ -1,6 +0,0 @@
-#2d array
-a.dtype # dtype of the elements in the array
-a = np.array([[1,2],[3,4],[5,6]], dtype = np.float64) # specify data type while creating an array
-print(a.itemsize)#itemsize means the number of items in the array along each dimension (in bytes)
-print(a.ndim)# ndim means number of dimensions in the array   
-print(a)
