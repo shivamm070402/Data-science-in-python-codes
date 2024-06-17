423210018c085af0b9dd90379ebddfcaa495b8f5
shivamm070402 <108880698+shivamm070402@users.noreply.github.com>
2024-06-16 23:39:13 -0700

    Delete shapeofarray.py

--- shapeofarray.py
+++
@@ -1,4 +0,0 @@
-#shape of array
-import numpy as np 
-a = np.array([[1,2],[3,4],[5,6]], dtype = np.float64) # specify data type while creating an array
-print(a.shape) # specify the shape of the array (rows and columns)
